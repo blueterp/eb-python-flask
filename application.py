a8f0ed5c5ca87a2b251ac959b07c8dd0f34678ed
Jonathan Howarth <jonathan.michael.howarth@gmail.com>
2023-08-04 08:02:05 -0400

    fix application

--- application.py
+++ application.py
@@ -1,25 +1,34 @@
-import flask
-import os
- 
-application = flask.Flask(__name__)
+from flask import Flask
 
-# Only enable Flask debugging if an env var is set to true
-application.debug = os.environ.get('FLASK_DEBUG') in ['true', 'True']
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
 
-# Get application version from env
-app_version = os.environ.get('APP_VERSION')
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
 
-# Get cool new feature flag from env
-enable_cool_new_feature = os.environ.get('ENABLE_COOL_NEW_FEATURE') in ['true', 'True']
+# EB looks for an 'application' callable by default.
+application = Flask(__name__)
 
-@application.route('/')
-def hello_world():
-    message = "Hello, world!"
-    return flask.render_template('index.html',
-                                  title=message,
-                                  flask_debug=application.debug,
-                                  app_version=app_version,
-                                  enable_cool_new_feature=enable_cool_new_feature)
- 
-if __name__ == '__main__':
-    application.run(host='0.0.0.0')
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
+
+# run the app.
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    application.debug = True
+    application.run()
